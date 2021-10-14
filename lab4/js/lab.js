03f577dc137bbf168675588636617e353e4d8af2
lyza13 <91556557+lyza13@users.noreply.github.com>
2021-10-13 21:09:51 -0700

    update to lab 4 index & js file

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+// Building a Wooden House in Minecraft.
+// Pseudocode to guide a game, Minecraft, to do a task: building a wooden house.
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Logan Flansaas and Lyza Stevens.
+// @since  10.13.2021
+
+//Build a wooden house in Minecraft:
+  //Locate trees
+  //Chop trees to acquire wood logs
+  //Turn wood logs into wood planks in crafting menu
+    //Return oak planks to inventory
+  //Determine location to build the house
+  //Plan out house shape, layout, and size
+  //Place wood planks to create the planned house
+    //If you do not have enough wooden planks to complete the house, repeat steps 1-3
+  //Lose the house to a creeper explosion
+  //Repeat
