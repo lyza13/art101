dc14876b1538132d6ef97a1cdd53345ae3274990
lyza13 <91556557+lyza13@users.noreply.github.com>
2021-10-18 13:27:33 -0700

    update to lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/* Lab 5: Data Types and Variables.
+* Created: 10.17.2021
+* @author Logan Flansaas and Lyza Stevens.
+* @since  10.19.2021
+* License: Public Domain.
+*/
+
+//Define Variables
+make = "Hyundai";
+model = "Elantra";
+color = "Black";
+year = 2017;
+
+// calculate
+age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
