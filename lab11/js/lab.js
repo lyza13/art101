3f5e7f02f3a35b46b7a4338b1b28bfc8ec8cad51
lyza13 <91556557+lyza13@users.noreply.github.com>
2021-11-08 20:25:05 -0800

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -5,18 +5,21 @@
 * License: Public Domain.
 */
 
+//Add a button to the challenge div
 $("#challenge").append("<button id='challenge-button'>Click me!</button>");
+//Add a button to the problems div
 $("#problems").append("<button id='problems-button'>Click me!</button>");
+//Add a button to the results div
 $("#results").append("<button id='results-button'>Click me!</button>");
-
+//Toggles the css styling of the challenge div
 $("#challenge-button").click(function(){
   $("#challenge").toggleClass("special");
 })
-
+//Toggles the css styling of the problems div
 $("#problems-button").click(function(){
   $("#problems").toggleClass("special");
 })
-
+//Toggles the css styling of the results div
 $("#results-button").click(function(){
   $("#results").toggleClass("special");
 })
