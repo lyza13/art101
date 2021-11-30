5cec98b825fbf64a7083b3e57def166bb0879eb5
lyza13 <91556557+lyza13@users.noreply.github.com>
2021-11-30 01:55:07 -0800

    update for lab15

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,4 +1,4 @@
-/* Lab 14: Loops
+/* Lab 14: Debugging Tools and Strategies
 * Created: 11.30.2021
 * @author Logan Flansaas and Lyza Stevens.
 * @since  11.30.2021
---
+++ lab15/js/lab.js
@@ -0,0 +1,29 @@
+/* Lab 15: AJAX
+* Created: 11.29.2021
+* @author Logan Flansaas and Lyza Stevens.
+* @since  11.29.2021
+* License: Public Domain.
+*/
+var myUrl = "http://numberapi.com/random/year"
+
+function callAjax() {
+// Using the core $.ajax() method
+$.ajax({
+    // API endpoint
+    url: myUrl,
+    // POST or GET request
+    type: "GET",
+})
+// If the request succeeds
+// data is passed back
+.done(function(data) {
+    console.log("Success:", data);
+    $("#output").html(data);
+})
+// If the request fails
+.fail(function(request,error) {
+    console.log(request, error);
+})
+}
+// calls AJAX on button click
+$("#activate").click(callAjax);
